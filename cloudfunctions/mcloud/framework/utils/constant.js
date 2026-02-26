e355900cec30ae8385bbbee38fe00d8608d9bf7a
zxwangbingbing@139.com <zxwangbingbing@139.com>
2026-02-26 20:48:06 +0800

    Cr

--- cloudfunctions/mcloud/framework/utils/constant.js
+++
@@ -1,8 +0,0 @@
- /**
- * Notes: 通用常量定义
- * Ver : CCMiniCloud Framework 2.32.1 ALL RIGHTS RESERVED BY cclinux0730 (wechat)
- * Date: 2020-09-05 04:00:00 
- */
-module.exports = {
- 
-}
--- cloudfunctions/mcloud/project/worklesson/public/constants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {    
-	// 首页推荐
-	SETUP_HOME_VOUCH_KEY : 'SETUP_HOME_VOUCH_KEY',
-}
